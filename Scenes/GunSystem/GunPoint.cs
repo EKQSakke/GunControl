3f8d7c743678dad80bb2d3707aee8fb91aac35e3
Sakari Ekqvist <ekq.sakke@gmail.com>
2024-12-30 00:49:42 +0200

    I don't get why the raycast is not picking the Area3D

--- Scenes/GunSystem/GunPoint.cs
+++ Scenes/GunSystem/GunPoint.cs
@@ -1,34 +1,38 @@
 public partial class GunPoint : Node3D
 {
-    MousePosition mousePosition;
-    Camera3D camera;
+	MousePosition mousePosition;
+	Camera3D camera;
+	MeshInstance3D rayMesh;
 
-    public override void _Ready()
-    {
-        mousePosition = this.AddChildOfType<MousePosition>();
-        camera = GetViewport().GetCamera3D();
-    }
+	public override void _Ready()
+	{
+		mousePosition = this.AddChildOfType<MousePosition>();
+		camera = GetViewport().GetCamera3D();
+		rayMesh = this.AddChildOfType<MeshInstance3D>();
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        LookAtMouse();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		LookAtMouse();
+	}
 
-    // Function that rotates this node to look at the mouse position	
-    public void LookAtMouse()
-    {
-        Vector2 mousePos = mousePosition.GetMousePosition();
-        Vector3 from = camera.ProjectRayOrigin(mousePos);
-        Vector3 to = from + camera.ProjectRayNormal(mousePos) * 1000;
+	// Function that rotates this node to look at the mouse position	
+	public void LookAtMouse()
+	{
+		var mousePos = mousePosition.GetMousePosition();
+		var worldPos = camera.ProjectPosition(mousePos, 1000);
 
-        var spaceState = GetWorld3D().DirectSpaceState;
-		var parameters = PhysicsRayQueryParameters3D.Create(from, to);
-        var result = spaceState.IntersectRay(parameters);
+		var spaceState = GetWorld3D().DirectSpaceState;
+		var parameters = PhysicsRayQueryParameters3D.Create(camera.GlobalPosition, worldPos, collisionMask: 1);
+		var result = spaceState.IntersectRay(parameters);
+		GD.Print($"{camera.GlobalPosition}, {worldPos}, {result.Count}");
 
-        if (result.Count > 0)
-        {
-            Vector3 point = (Vector3)result["position"];
-            LookAt(point, Vector3.Up);
-        }
-    }
+		if (result.TryGetValue("position", out var position))
+		{
+			var point = (Vector3)position;
+			LookAt(point, Vector3.Up);
+		}
+
+		DrawTools.DrawRay(rayMesh, camera.GlobalPosition, worldPos);
+	}
 }
