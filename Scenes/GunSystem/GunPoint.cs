9f5e1cdf551d530249313a8a1d7b4a242cdb0b76
Sakari Ekqvist <ekq.sakke@gmail.com>
2024-12-30 00:25:54 +0200

    LookAt

--- Scenes/GunSystem/GunPoint.cs
+++ Scenes/GunSystem/GunPoint.cs
@@ -1,20 +1,34 @@
 public partial class GunPoint : Node3D
 {
-	MousePosition mousePosition;
+    MousePosition mousePosition;
+    Camera3D camera;
 
-	public override void _Ready()
-	{
-		mousePosition = this.AddChildOfType<MousePosition>();
-	}
+    public override void _Ready()
+    {
+        mousePosition = this.AddChildOfType<MousePosition>();
+        camera = GetViewport().GetCamera3D();
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		LookAtMouse();
-	}
+    public override void _PhysicsProcess(double delta)
+    {
+        LookAtMouse();
+    }
 
-	// Function that rotates this node to look at the mouse position	
-	public void LookAtMouse()
-	{
+    // Function that rotates this node to look at the mouse position	
+    public void LookAtMouse()
+    {
+        Vector2 mousePos = mousePosition.GetMousePosition();
+        Vector3 from = camera.ProjectRayOrigin(mousePos);
+        Vector3 to = from + camera.ProjectRayNormal(mousePos) * 1000;
 
-	}
+        var spaceState = GetWorld3D().DirectSpaceState;
+		var parameters = PhysicsRayQueryParameters3D.Create(from, to);
+        var result = spaceState.IntersectRay(parameters);
+
+        if (result.Count > 0)
+        {
+            Vector3 point = (Vector3)result["position"];
+            LookAt(point, Vector3.Up);
+        }
+    }
 }
